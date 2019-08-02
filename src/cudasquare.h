953db325cf103de1cfaf44e0161dabb1926b81e3
Rjected <kidscline01@gmail.com>
2019-08-02 11:06:05 -0400

    Added create_instance

--- src/cudasquare.h
+++ src/cudasquare.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <gmp.h>
 
-__host__ void launch_repeat_square(mpz_t*, const uint64_t*, const uint64_t*, const mpz_t*, const uint64_t);
+template<class params>
+__host__ void launch_repeat_square(mpz_t* result, const uint64_t* a, const uint64_t* t, const mpz_t* N, const uint64_t len);
 
 template<class params>
 __global__ void kernel_repeat_square();
