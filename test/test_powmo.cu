582cd7bd62a4dd8bd1ee776aed34ada10b431236
Rjected <kidscline01@gmail.com>
2019-08-02 16:09:37 -0400

    Just put rsa in header
     * I might as well make the linker happy

--- test/test_powmo.cu
+++ test/test_powmo.cu
@@ -1,12 +1,21 @@
 #include <gtest/gtest.h>
+#include <sstream>
+#include <string>
+#include <chrono>
 #include <time.h>
 #include "../src/powm_odd.cu"
 int main() {
   typedef powm_params_t<8, 1024, 5> params;
-  printf("[%s]: Running normal test...\n", time(NULL));
+  std::stringstream ss;
+  time_t now;
+  now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+  ss << std::put_time(localtime(&now), "%y-%m-%d %h:%m:%s");
+  printf("[%s]: Running normal test\n", ss.str());
   run_test<params>(10000);
   typedef powm_params_t<16, 4096, 5> params_puzzle;
-  printf("[%s]: Running puzzle test...\n", time(NULL));
+  now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+  ss << std::put_time(localtime(&now), "%y-%m-%d %h:%m:%s");
+  printf("[%s]: Running puzzle test\n", ss.str());
   run_puzzle_test<params_puzzle>(10000, 1024);
   return 0;
 }
