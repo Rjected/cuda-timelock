bc13f1c4b32819fde582f3304d42bf12c74279c5
Rjected <kidscline01@gmail.com>
2019-09-19 00:01:10 -0400

    bigger param

--- test/test_powmo.cu
+++ test/test_powmo.cu
@@ -3,7 +3,7 @@
 int main() {
   typedef powm_params_t<8, 1024, 5> params;
   run_test<params>(128);
-  typedef powm_params_t<32, 4096, 16> params_puzzle;
-  run_puzzle_test<params_puzzle>(1000000, 4096);
+  typedef powm_params_t<32, 16384, 16> params_puzzle;
+  run_puzzle_test<params_puzzle>(100, 15000);
   return 0;
 }
