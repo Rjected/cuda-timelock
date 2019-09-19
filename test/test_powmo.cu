8f407edbcaf7b6a95eaffb68e1496efad95e2cd0
Rjected <kidscline01@gmail.com>
2019-09-18 22:58:58 -0400

    biiiig exponent

--- test/test_powmo.cu
+++ test/test_powmo.cu
@@ -3,7 +3,7 @@
 int main() {
   typedef powm_params_t<8, 1024, 5> params;
   run_test<params>(128);
-  typedef powm_params_t<16, 4096, 5> params_puzzle;
+  typedef powm_params_t<16, 32768, 5> params_puzzle;
   run_puzzle_test<params_puzzle>(100, 25000);
   return 0;
 }
